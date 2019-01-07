3f48ee16ae028d5e326a71e350bc9b2a4e65124f
5k2k <devendra.d077@slcstudents.org>
2019-01-07 10:14:31 -0700

    added

--- myScript.js
+++ myScript.js
@@ -4,13 +4,10 @@ function myFunction () {
    document.getElementById('demo').innerHTML = "Paragraph Changed";
 }
 
-//Defining variables
-var x = 
 
 
 //This function changes the text in the math paragraph
 
-function mathFunction (x,y){
-    return 10 + 6;
-    document.getElementById('math').innerHTML = mathFunction(x,y);
-}
+function mathFunction (x,y) {
+    return x + y;
+    }
